ef45d00a1677742052d2d0607f3b0c19b965d522
m <mingh4hire@gmail.com>
2022-09-10 00:08:10 -0500

    test

---
+++ src/main/java/com/example/demo/Runit.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Runit {
+    @RequestMapping("add")
+    public String add(int a, int b){
+        return "a" + a + b;
+    }
+    @RequestMapping("/")
+    public String aaa(){
+        return "a";
+    }
+    @RequestMapping("/bbbb")
+    public String aaab(Integer a){
+
+
+        return Math.pow(a,2) + "";
+    }
+
+    @RequestMapping("")
+    public String nbnb(){
+        return "b";
+    }
+    @RequestMapping("add2")
+    public String add2(int a, int b){
+        return "a" + a + b;
+    }
+    @RequestMapping("/add3")
+    public String add3(int a, int b){
+        return "a" + a + b;
+    }
+
+
+}
